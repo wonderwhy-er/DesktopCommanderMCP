eb87e3bfc6ee70f357842816479c997fbe93b9f6
serg33v <serg33v@gmail.com>
2025-05-29 14:36:35 +0100

    chore: grammar

--- setup-claude-server.js
+++ setup-claude-server.js
@@ -484,7 +484,7 @@ async function restartClaude() {
                 logToFile('\nTo use the server restart Claude if it\'s currently running\n');
             }
             
-            logToFile("\n✅ Installation sucessfully completed! Thank you for using Desktop Commander!\n");
+            logToFile("\n✅ Installation successfully completed! Thank you for using Desktop Commander!\n");
             logToFile('\nThe server is available as "desktop-commander" in Claude\'s MCP server list');
             
             logToFile("Future updates will install automatically — no need to run this setup again.\n\n");
