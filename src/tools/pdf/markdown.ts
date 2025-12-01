b259461f0c9802eb5325fddb63f05fe5be968cac
Eduard Ruzga <wonderwhy.er@gmail.com>
2025-12-01 14:17:20 +0200

    Revert "Update src/tools/pdf/markdown.ts"
    This reverts commit 134047f4f5f624491efdfdad27bf5112fbadf74f.

--- src/tools/pdf/markdown.ts
+++ src/tools/pdf/markdown.ts
@@ -23,10 +23,8 @@ async function loadPdfToBuffer(source: string): Promise<Buffer | ArrayBuffer> {
 export async function parsePdfToMarkdown(source: string, pageNumbers: number[] | PageRange = []): Promise<PdfParseResult> {
     try {
         const data = await loadPdfToBuffer(source);
-        const byteData = data instanceof Uint8Array ? data : new Uint8Array(data);
 
         // @ts-ignore: Type definition mismatch for ESM usage
-        return await pdf2md(byteData, pageNumbers);
         return await pdf2md(new Uint8Array(data), pageNumbers);
 
     } catch (error) {
